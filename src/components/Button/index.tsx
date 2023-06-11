aa053195f9bc2a1487416fb3f3967b859021e815
blukoo <dnehdrkd123@naver.com>
2023-06-11 20:31:25 +0900

    [feat]button 에 debounce 수정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,28 +1,31 @@
-import { debounce } from "@/utils/utils";
 import React, { useCallback, useEffect, useRef } from "react";
 import useDebounce from "@/hooks/useDebounce";
 function Button(props) {
   const { onClick, style, children, useDebounce = true } = props;
   const buttonRef = useRef(null);
-  var timer;
-  const _onClick = useCallback(() => {
-    if (buttonRef && buttonRef.current && useDebounce) {
-      buttonRef.current.addEventListener("click", function () {
-        if (timer) {
-          clearTimeout(timer);
-        }
-        timer = setTimeout(function () {
-          console.log("여기에 ajax 요청");
-          onClick();
-        }, 5000);
-      });
-    }
-    onClick();
+  let timeout;
+  const debounce = useCallback((func, ms) => {
+    console.log("ss22");
+    // 현재 타이머가 실행되고 있으면 timeout에 타이머의 id를 저장한다.
+
+    return () => {
+      // 만약 타이머가 실행되고 있으면 현재 타이머를 지운다.
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+
+      // timeout에 새로운 타이머를 할당한다. 만약 ms만큼 시간이 지났으면 함수를 실행한다.
+      timeout = setTimeout(() => {
+        console.log(timeout, "timpout");
+        func();
+      }, ms);
+    };
   }, []);
+
   return (
-    <button ref={buttonRef} style={style} onClick={_onClick}>
+    <button ref={buttonRef} style={style} onClick={debounce(onClick, 3000)}>
       {children}
     </button>
   );
 }
-export default Button;
+export default React.memo(Button);
