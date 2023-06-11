bb6569d44d18a7ff60efb4b15e087cbe19984ac6
blukoo <dnehdrkd123@naver.com>
2023-06-11 16:41:01 +0900

    [feat]리다이렉션 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFounds from '@/pages/NotFounds';
-import Home from '@/pages/Home';
-import TempPage from '@/pages/TempPage';
-import TestPage2 from '@/pages/TestPages/TestPage2';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+  Navigate
+} from "react-router-dom";
+import NotFounds from "@/pages/NotFounds";
+import Home from "@/pages/Home";
+import TempPage from "@/pages/TempPage";
+import NeedLoginPage from "@/pages/NeedLoginPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 // import ProtectedRoute from '@/components/ProtectedRoute';
 const router = createBrowserRouter([
   {
-    path: '/',
+    index: true, // <-- match on parent, i.e. "/"
+    element: <Navigate to="/home" replace /> // <-- redirect
+  },
+
+  {
+    path: "/",
     element: <App></App>,
     errorElement: <NotFounds />,
     children: [
       {
-        index: true,
+        path: "home",
         element: <Home />
       },
       {
+        element: <TempPage />,
+        path: "temp"
+      },
+      {
         element: (
-          // <ProtectedRoute>
-          <TempPage />
-        )
-        // </ProtectedRoute>
+          <ProtectedRoute>
+            <NeedLoginPage />
+          </ProtectedRoute>
+        ),
+        path: "needLogin"
       }
     ]
   }
 ]);
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
