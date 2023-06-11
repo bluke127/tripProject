615e4ec782468b5376b3920bfe61f767af7cfd12
blukoo <dnehdrkd123@naver.com>
2023-06-11 15:47:38 +0900

    [fix]기타 세팅사항

--- craco.config.js
+++ craco.config.js
@@ -7,8 +7,19 @@ module.exports = {
       options: {
         source: "tsconfig",
         baseUrl: "./src",
-        tsConfigPath: "./tsconfig.paths.json",
-      },
-    },
+        tsConfigPath: "./tsconfig.paths.json"
+      }
+    }
   ],
+  style: {
+    sass: {
+      loaderOptions: {
+        additionalData: `
+            @import "@/styles/_variables.scss";
+            @import "@/styles/_utils.scss";
+            @import "@/styles/Init.scss";
+        `
+      }
+    }
+  }
 };
