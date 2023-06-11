dbf49113ee70837677fd677f5ec7bc3240bcc647
blukoo <dnehdrkd123@naver.com>
2023-06-11 12:22:12 +0900

    [feat]axios 및 util함수 추가

---
+++ src/utils/utils.ts
@@ -0,0 +1,23 @@
+export const handleNumberFormat = (
+  number?: any | number | bigint,
+  notation:
+    | "compact"
+    | "standard"
+    | "scientific"
+    | "engineering"
+    | undefined = "compact",
+) =>{
+  let _num=number.toString().replace(/[^0-9]/g,'')
+  let v=new Intl.NumberFormat("ko", {
+    notation:notation,
+  }).format(_num);
+  console.log(v,"vvv")
+  return v?v:'0'
+}
+export const handleDateFormat = (
+  date?: any | number | bigint,
+  dateStyle: "long" | "full" | "medium" | "short" | undefined = "long",
+) =>
+  new Intl.DateTimeFormat("ko", {
+    dateStyle,
+  }).format(date ?? new Date());
