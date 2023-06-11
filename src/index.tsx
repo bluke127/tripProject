4441c3b74471496acac7f4703f48870cab7eadca
blukoo <dnehdrkd123@naver.com>
2023-06-11 12:20:48 +0900

    [feat]라우트 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFounds from '@/pages/NotFounds';
+import Home from '@/pages/Home';
+import TempPage from '@/pages/TempPage';
+import TestPage2 from '@/pages/TestPages/TestPage2';
+// import ProtectedRoute from '@/components/ProtectedRoute';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+    errorElement: <NotFounds />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        element: (
+          // <ProtectedRoute>
+          <TempPage />
+        )
+        // </ProtectedRoute>
+      }
+    ]
+  }
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
